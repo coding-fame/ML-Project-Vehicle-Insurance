edc274c421f00528c1aba6228ae4794fdb948b63
coding-fame <gaajanana@gmail.com>
2025-02-02 11:43:40 +0530

    Logger module created and sucessfully demostrated in demo.py

---
+++ demo.py
@@ -0,0 +1,8 @@
+from src.logger import logging
+
+logging.info("Hello, world!")
+logging.debug("This is a debug message.")
+logging.warning("This is a warning message.")
+logging.error("This is an error message.")
+logging.critical("This is a critical message.")
+logging.exception("This is an exception message.")
